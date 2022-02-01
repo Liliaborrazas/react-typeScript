05414562a6eafc23ab4cbf0ca08682731f886225
Lilia Borrazás Pristupa <lilia.borrazas@babelgroup.com>
2022-02-01 11:30:33 +0100

    Aprendiendo typescript con react, custom hooks, useState, useReducer, etc

---
+++ src/components/Timer.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from 'react';
+
+type TimerProps = {
+    milisegundos: number
+}
+export const Timer = ( {milisegundos}: TimerProps) => {
+    
+    const [segundos, setSegundos ] = useState(0);
+    const ref = useRef<NodeJS.Timeout>();
+
+    useEffect(()=> {
+        ref.current && clearInterval( ref.current);
+        ref.current = setInterval(()=> setSegundos(s => s + 1), milisegundos);
+    },[milisegundos])
+
+  return(
+      <>
+        <h4>Timer: <small>{segundos}</small></h4>
+      </>
+
+  ) 
+};
